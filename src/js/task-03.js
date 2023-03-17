72151e5077b968ecf684967d1cbba93c7086a6e8
smiadv2022 <msorokina@spacemedia.com.ua>
2023-03-18 01:16:58 +0200

    task-03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,13 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const galleryUl=document.querySelector(".gallery");
+galleryUl.style.display="flex";
+galleryUl.style.listStyle="none";
+galleryUl.style.gap="20px";
+
+const galleryList =images.map( function ({url, alt}){
+let img;
+return img=`<li> <img src=${url} alt=${alt} width=320px height=210px /></li>`;
+}).join(" ");
+galleryUl.insertAdjacentHTML("beforeend", galleryList);
