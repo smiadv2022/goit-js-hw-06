c4d98805b80d4ce5d1863cf223116e56152d5771
smiadv2022 <msorokina@spacemedia.com.ua>
2023-03-18 00:39:05 +0200

    Task-02

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,14 +1,8 @@
 const categ = document.querySelectorAll(".item");
-console.log("Number of categories: ", categ.length)
-// console.dir(categ)
+console.log("Number of categories: ", categ.length);
 categ.forEach(el => {
   console.log("Category: ", el.firstElementChild.innerHTML);
-  // const categ1 = el.querySelectorAll("li");
-  console.log("Elements: ", el.querySelectorAll("li").length)
+  console.log("Elements: ", el.querySelectorAll("li").length);
 // console.log("Elements: ", el.lastElementChild.children.length);
   
 });
-// categ.forEach (el1=>{
-//   const categ1 = el1.querySelectorAll("li");
-//   console.log(categ1)
-// });
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsUl=document.querySelector("#ingredients");
+const  ingredientsArr= ingredients.map(ingredient=>{
+  const li = document.createElement("li");
+  li.textContent=ingredient;
+  li.classList="item";
+  return li;
+});
+
+ingredientsUl.append(...ingredientsArr);
